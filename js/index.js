62841c07dd4e3316960925bbeb6850c1bf62123c
xoxosoii <puiuxq@icloud.com>
2021-12-19 22:37:26 +0900

    :rocket: Add project

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function ani_parallax() {
+    // let ani = document.querySelectorAll('.prlx');
+    //
+    // function parallax() {
+    //     Array.prototype.forEach.call(ani, function(item) {
+    //         item.classList.add('active');
+    //     })
+    // }
+    // function event_listener() {
+    //     window.addEventListener('load', parallax);
+    // }
+    //
+    // function init() {
+    //     event_listener();
+    // }
+    //
+    // return init();
+}
+
+new ani_parallax;
